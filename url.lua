806ba886b23ff4296bb189f4daa2ff56240281f5
Adrian L Lange <adrianlund@gmail.com>
2010-06-10 20:06:50 +0200

    Add url highlight module

---
+++ url.lua
@@ -0,0 +1,32 @@
+﻿local patterns = {
+	'(http://%S+)',
+	'(www%.%S+)',
+	'(%d+%.%d+%.%d+%.%d+:?%d*)',
+}
+
+for _, event in pairs({
+	'CHAT_MSG_GUILD',
+	'CHAT_MSG_PARTY',
+	'CHAT_MSG_RAID',
+	'CHAT_MSG_WHISPER',
+	'CHAT_MSG_SAY'
+}) do
+	ChatFrame_AddMessageEventFilter(event, function(self, event, str, ...)
+		for _, pattern in pairs(patterns) do
+			local result, match = string.gsub(str, pattern, '|cff80c8fe|Hurl:%1|h[%1]|h|r')
+			if(match > 0) then
+				return false, result, ...
+			end
+		end
+	end)
+end
+
+local orig = SetItemRef
+function SetItemRef(link, str, ...)
+	if(string.sub(link, 1, 3) ~= 'url') then return orig(link, str, ...) end
+
+	ChatFrameEditBox:Insert(string.sub(link, 5))
+	ChatFrameEditBox:HighlightText()
+	ChatFrameEditBox:SetFocus()
+	ChatFrameEditBox:Show()
+end
