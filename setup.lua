1980e53e8d5db889853bcced4d4a6e2f49b06626
p3lim <adrianlund@gmail.com>
2010-09-20 18:34:00 +0200

    Don't wait for the CombatLog to load

--- setup.lua
+++ setup.lua
@@ -1,67 +1,57 @@
 ﻿local TEXTURE = [=[Interface\ChatFrame\ChatFrameBackground]=]
 local BACKDROP = {bgFile = TEXTURE, edgeFile = TEXTURE, edgeSize = 1}
+local FONT = [=[Interface\AddOns\Gibberish\semplice.ttf]=]
 
-local addon = CreateFrame('Frame')
-addon:RegisterEvent('ADDON_LOADED')
-addon:SetScript('OnEvent', function(self, event, name)
-	if(name ~= 'Blizzard_CombatLog') then return end
+for index = 1, 5 do
+	local frame = _G['ChatFrame'..index]
+	frame:SetFont(FONT, 8, 'OUTLINEMONOCHROME')
+	frame:SetShadowOffset(0, 0)
+	frame:SetClampRectInsets(0, 0, 0, 0)
 
-	for index = 1, 5 do
-		local frame = _G['ChatFrame'..index]
-		frame:SetFont([=[Interface\AddOns\Gibberish\semplice.ttf]=], 8, 'OUTLINEMONOCHROME')
-		frame:SetShadowOffset(0, 0)
-		frame:SetClampRectInsets(0, 0, 0, 0)
+	local bg = CreateFrame('Frame', nil, frame)
+	bg:SetPoint('TOPRIGHT', 2, 0)
+	bg:SetPoint('BOTTOMLEFT', -2, -4)
+	bg:SetBackdrop(BACKDROP)
+	bg:SetBackdropColor(0, 0, 0, 0.3)
+	bg:SetBackdropBorderColor(0, 0, 0)
+	bg:SetFrameLevel(0)
 
-		local bg = CreateFrame('Frame', nil, frame)
-		bg:SetPoint('TOPRIGHT', 2, 0)
-		bg:SetPoint('BOTTOMLEFT', -2, -4)
-		bg:SetBackdrop(BACKDROP)
-		bg:SetBackdropColor(0, 0, 0, 0.3)
-		bg:SetBackdropBorderColor(0, 0, 0)
-		bg:SetFrameLevel(0)
+	local editbox = _G['ChatFrame'..index..'EditBox']
+	editbox:ClearAllPoints()
+	editbox:SetPoint('BOTTOMRIGHT', 0, -20)
+	editbox:SetPoint('TOPLEFT', frame, 'BOTTOMLEFT', 0, -6)
+	editbox:SetFont(FONT, 8, 'OUTLINEMONOCHROME')
+	editbox:SetShadowOffset(0, 0)
+	editbox:SetAltArrowKeyMode(false)
 
-		local editbox = _G['ChatFrame'..index..'EditBox']
-		editbox:ClearAllPoints()
-		editbox:SetPoint('TOPLEFT', frame, 'BOTTOMLEFT', 0, -6)
-		editbox:SetPoint('TOPRIGHT', frame, 'BOTTOMRIGHT', 0, -6)
-		editbox:SetFont([=[Interface\AddOns\Gibberish\semplice.ttf]=], 8, 'OUTLINEMONOCHROME')
-		editbox:SetShadowOffset(0, 0)
-		editbox:SetAltArrowKeyMode(false)
-		editbox:SetHeight(14)
+	editbox.focusLeft:SetTexture(nil)
+	editbox.focusMid:SetTexture(nil)
+	editbox.focusRight:SetTexture(nil)
 
-		editbox.focusLeft:SetTexture(nil)
-		editbox.focusMid:SetTexture(nil)
-		editbox.focusRight:SetTexture(nil)
-		editbox.header:Hide()
+	editbox.header:SetFont(FONT, 8, 'OUTLINEMONOCHROME')
 
-		local bg = CreateFrame('Frame', nil, editbox)
-		bg:SetPoint('TOPRIGHT', 2, 0)
-		bg:SetPoint('BOTTOMLEFT', -2, -2)
-		bg:SetBackdrop(BACKDROP)
-		bg:SetBackdropColor(0, 0, 0, 0.3)
-		bg:SetBackdropBorderColor(0, 0, 0)
-		bg:SetFrameLevel(0)
+	local bg = CreateFrame('Frame', nil, editbox)
+	bg:SetPoint('TOPRIGHT', 2, 0)
+	bg:SetPoint('BOTTOMLEFT', -2, -2)
+	bg:SetBackdrop(BACKDROP)
+	bg:SetBackdropColor(0, 0, 0, 0.3)
+	bg:SetBackdropBorderColor(0, 0, 0)
+	bg:SetFrameLevel(0)
 
-		_G['ChatFrame'..index..'ButtonFrame']:Hide()
-		_G['ChatFrame'..index..'EditBoxLeft']:SetTexture(nil)
-		_G['ChatFrame'..index..'EditBoxMid']:SetTexture(nil)
-		_G['ChatFrame'..index..'EditBoxRight']:SetTexture(nil)
-	end
-
-	DEFAULT_CHATFRAME_ALPHA = 0
-	ChatFrameMenuButton:SetAlpha(0)
-	ChatFrameMenuButton:EnableMouse(false)
-	FriendsMicroButton:Hide()
-
-	ChatTypeInfo.CHANNEL.sticky = 0
-	ChatTypeInfo.WHISPER.sticky = 0
-	ChatTypeInfo.BN_WHISPER.sticky = 0
-end)
-
-function ChatEdit_UpdateHeader(editbox)
-	local info = ChatTypeInfo[editbox:GetAttribute('chatType')]
-	editbox:SetTextColor(info.r, info.g, info.b)
+	_G['ChatFrame'..index..'ButtonFrame']:Hide()
+	_G['ChatFrame'..index..'EditBoxLeft']:SetTexture(nil)
+	_G['ChatFrame'..index..'EditBoxMid']:SetTexture(nil)
+	_G['ChatFrame'..index..'EditBoxRight']:SetTexture(nil)
 end
+
+DEFAULT_CHATFRAME_ALPHA = 0
+ChatFrameMenuButton:SetAlpha(0)
+ChatFrameMenuButton:EnableMouse(false)
+FriendsMicroButton:Hide()
+
+ChatTypeInfo.CHANNEL.sticky = 0
+ChatTypeInfo.WHISPER.sticky = 0
+ChatTypeInfo.BN_WHISPER.sticky = 0
 
 function FloatingChatFrame_OnMouseScroll(self, direction)
 	if(direction > 0) then
