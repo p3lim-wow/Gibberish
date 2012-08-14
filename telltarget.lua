8f4ef56489c3c3fab72d506a9c237189b25c3eff
Adrian L Lange <adrianlund@gmail.com>
2012-08-14 19:45:42 +0200

    Apparently, you cannot cooperate with yourself

--- telltarget.lua
+++ telltarget.lua
@@ -1,5 +1,5 @@
 ﻿hooksecurefunc('ChatEdit_OnSpacePressed', function(self)
-	if(string.sub(self:GetText(), 1, 3) == '/tt' and UnitCanCooperate('player', 'target')) then
+	if(string.sub(self:GetText(), 1, 3) == '/tt' and (UnitCanCooperate('player', 'target') or UnitIsUnit('player', 'target'))) then
 		ChatFrame_SendTell(GetUnitName('target', true):gsub(' ', ''))
 	end
 end)
