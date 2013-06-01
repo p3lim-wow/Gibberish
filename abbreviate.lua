3b3a8f09e862ec119f55128fbafc07a6970fb3d1
Adrian L Lange <adrianlund@gmail.com>
2013-06-02 01:16:03 +0200

    Some optimizations

--- abbreviate.lua
+++ abbreviate.lua
@@ -1,4 +1,9 @@
-﻿local hooks = {}
+﻿local string_gsub = string.gsub
+local string_find = string.find
+local string_split = string.split
+local string_format = string.format
+
+local hooks = {}
 local abbrev = {
 	INSTANCE_CHAT = 'i',
 	OFFICER = 'o',
@@ -24,38 +29,43 @@ do
 end
 
 local function Strip(info, name)
-	return string.format('|Hplayer:%s|h%s|h', info, name:gsub('%-[^|]+', ''))
+	return string_format('|Hplayer:%s|h%s|h', info, string_gsub(name, '%-[^|]+', ''))
 end
 
-local function Abbreviate(channel)
-	return string.format('|Hchannel:%s|h%s|h', channel, abbrev[channel] or channel:gsub('channel:', ''))
+local function Abbreviate(channel, name)
+	local leader = ''
+	if(string_find(name, LEADER)) then
+		leader = '|cffffff00!|r'
+	end
+
+	return string_format('|Hchannel:%s|h%s|h %s', channel, abbrev[channel] or string_gsub(channel, 'channel:', ''), leader)
 end
 
 local function BattleNet(info, name)
-	local _, presence = string.split(':', info)
+	local _, presence = string_split(':', info)
 
 	local _, toon, client, _, _, _, _, class = BNGetFriendToonInfo(BNGetFriendIndex(presence), 1)
 
 	if(client == BNET_CLIENT_WOW) then
 		local colors = RAID_CLASS_COLORS[classes[class]]
-		return string.format('|HBNplayer:%s|h|c%s%s|r|h', info, colors.colorStr, toon)
+		return string_format('|HBNplayer:%s|h|c%s%s|r|h', info, colors.colorStr, toon)
 	elseif(client == BNET_CLIENT_D3) then
-		return string.format('|HBNplayer:%s|h|TInterface\\ChatFrame\\UI-ChatIcon-D3:15:15:-2:-1:72:72:16:58:16:58|t|cffB71709%s|r|h', info, toon)
+		return string_format('|HBNplayer:%s|h|TInterface\\ChatFrame\\UI-ChatIcon-D3:15:15:-2:-1:72:72:16:58:16:58|t|cffB71709%s|r|h', info, toon)
 	elseif(client == BNET_CLIENT_SC2) then
-		return string.format('|HBNplayer:%s|h|TInterface\\ChatFrame\\UI-ChatIcon-SC2:15:15:-1:-1:72:72:16:58:16:58|t|cff00B6FF%s|r|h', info, toon)
+		return string_format('|HBNplayer:%s|h|TInterface\\ChatFrame\\UI-ChatIcon-SC2:15:15:-1:-1:72:72:16:58:16:58|t|cff00B6FF%s|r|h', info, toon)
 	end
 end
 
 local function AddMessage(self, message, ...)
-	message = message:gsub('|Hplayer:(.-)|h%[(.-)%]|h', Strip)
-	message = message:gsub('|HBNplayer:(.-)|h%[(.-)%]|h', BattleNet)
-	message = message:gsub('|Hchannel:(.-)|h%[(.-)%]|h', Abbreviate)
+	message = string_gsub(message, '|Hplayer:(.-)|h%[(.-)%]|h', Strip)
+	message = string_gsub(message, '|HBNplayer:(.-)|h%[(.-)%]|h', BattleNet)
+	message = string_gsub(message, '|Hchannel:(.-)|h%[(.-)%]|h ', Abbreviate)
 
-	message = message:gsub('^To (.-|h)', '|cffA1A1A1@|r%1')
-	message = message:gsub('^(.-|h) whispers', '%1')
-	message = message:gsub('^(.-|h) says', '%1')
-	message = message:gsub('^(.-|h) yells', '%1')
-	message = message:gsub('^%['..RAID_WARNING..'%]', 'w')
+	message = string_gsub(message, '^To (.-|h)', '|cffA1A1A1@|r%1')
+	message = string_gsub(message, '^(.-|h) whispers', '%1')
+	message = string_gsub(message, '^(.-|h) says', '%1')
+	message = string_gsub(message, '^(.-|h) yells', '%1')
+	message = string_gsub(message, '^%['..RAID_WARNING..'%]', 'w')
 
 	return hooks[self](self, message, ...)
 end
