3406881a19efa0f7d81a6dda98f4742f35ae62b4
Adrian L Lange <adrianlund@gmail.com>
2014-08-19 20:01:28 +0200

    Scan for urls in more types of messages

--- url.lua
+++ url.lua
@@ -8,14 +8,27 @@
 local numPatterns = #patterns
 
 for _, event in next, {
+	'CHAT_MSG_BN_CONVERSATION',
+	'CHAT_MSG_BN_WHISPER',
+	'CHAT_MSG_BN_WHISPER_INFORM',
+	'CHAT_MSG_CHANNEL',
 	'CHAT_MSG_GUILD',
+	'CHAT_MSG_INSTANCE_CHAT',
+	'CHAT_MSG_INSTANCE_CHAT_LEADER',
+	'CHAT_MSG_OFFICER',
 	'CHAT_MSG_PARTY',
+	'CHAT_MSG_PARTY_LEADER',
 	'CHAT_MSG_RAID',
 	'CHAT_MSG_RAID_LEADER',
 	'CHAT_MSG_CHANNEL',
+	'CHAT_MSG_RAID_WARNING',
+	'CHAT_MSG_SAY',
+	'CHAT_MSG_SYSTEM',
 	'CHAT_MSG_WHISPER',
 	'CHAT_MSG_BN_WHISPER',
-	'CHAT_MSG_SAY'
+	'CHAT_MSG_SAY',
+	'CHAT_MSG_WHISPER_INFORM',
+	'CHAT_MSG_YELL'
 } do
 	ChatFrame_AddMessageEventFilter(event, function(self, event, str, ...)
 		for index = 1, numPatterns do
