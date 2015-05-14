2615943cf7cd969ad7a9a74b43aac20f7ca7e9ae
Adrian L Lange <mail@p3lim.net>
2015-05-14 15:55:17 +0200

    Let the GM decide when the conversation is over

--- gm.lua
+++ gm.lua
@@ -7,6 +7,22 @@ local function CreateWindow()
 	FCF_SelectDockFrame(SELECTED_CHAT_FRAME)
 
 	ns.Skin(frame:GetID())
+	PlaySound('TellMessage', 'Master')
+end
+
+local function AddMessage(body, outgoing)
+	if(not outgoing) then
+		if(not frame) then
+			CreateWindow()
+		end
+
+		if(SELECTED_CHAT_FRAME ~= frame) then
+			FCF_StartAlertFlash(frame)
+		end
+	end
+
+	local info = ChatTypeInfo.WHISPER
+	frame:AddMessage(body, info.r, info.g, info.b)
 end
 
 local link = [[|TInterface\ChatFrame\UI-ChatIcon-Blizz:14:22:-1:-2:32:16:4:26:0:16|t |cff0090ff|HplayerGM:%s|h%s|h|r]]
@@ -17,46 +33,32 @@ local outMessage = '|cffa1a1a1@|r' .. inMessage
 local Handler = CreateFrame('Frame')
 Handler:RegisterEvent('CHAT_MSG_WHISPER')
 Handler:RegisterEvent('CHAT_MSG_WHISPER_INFORM')
+Handler:RegisterEvent('UPDATE_WEB_TICKET')
 Handler:RegisterEvent('PLAYER_LOGIN')
 Handler:SetScript('OnEvent', function(self, event, ...)
 	local message, name, _, _, _, flag = ...
 	if(event == 'CHAT_MSG_WHISPER' and flag == 'GM') then
-		if(not frame) then
-			CreateWindow()
-		end
-
-		if(SELECTED_CHAT_FRAME ~= frame) then
-			FCF_StartAlertFlash(frame)
-		end
-
+		AddMessage(string.format(inMessage, name, name, message))
 		GibberishLastGM = name
-
-		local info = ChatTypeInfo.WHISPER
-		local body = string.format(inMessage, name, name, message)
-		frame:AddMessage(body, info.r, info.g, info.b)
-
 		ChatEdit_SetLastTellTarget(name, 'WHISPER')
 	elseif(event == 'CHAT_MSG_WHISPER_INFORM' and flag == 'GM') then
-		local info = ChatTypeInfo.WHISPER
-		local body = string.format(outMessage, name, name, message)
-		frame:AddMessage(body, info.r, info.g, info.b)
-	elseif(event == 'PLAYER_LOGIN' and GibberishLastGM) then
-		CreateWindow()
-
-		if(SELECTED_CHAT_FRAME ~= frame) then
-			FCF_StartAlertFlash(frame)
+		AddMessage(string.format(outMessage, name, name, message), true)
+	elseif(event == 'UPDATE_WEB_TICKET') then
+		local hasTicket, numTickets, ticketStatus, caseIndex = ...
+		if(ticketStatus == LE_TICKET_STATUS_NMI) then
+			AddMessage(TICKET_STATUS_NMI)
+		elseif(frame and (not ticketStatus or ticketStatus >= 2)) then
+			AddMessage('Your ticket has been closed.')
+			GibberishLastGM = nil
 		end
-
-		local info = ChatTypeInfo.WHISPER
-		local body = string.format(GM_CHAT_LAST_SESSION, string.format(link, GibberishLastGM, GibberishLastGM))
-		frame:AddMessage(body, info.r, info.g, info.b)
+	elseif(event == 'PLAYER_LOGIN' and GibberishLastGM) then
+		AddMessage(string.format(GM_CHAT_LAST_SESSION, string.format(link, GibberishLastGM, GibberishLastGM)))
 	end
 end)
 
 hooksecurefunc('FCF_Tab_OnClick', function(self, button)
 	if(frame and button == 'MiddleButton') then
 		if(self:GetID() == frame:GetID()) then
-			GibberishLastGM = nil
 			frame = nil
 		end
 	end
